45e5608ef2d1e8dd7495f415ea49073c462bfbc2
Kevin <jsgu@aceinna.com>
2018-08-06 15:48:31 +0800

    init httpTest

---
+++ httpTest/AzureToAns.py
@@ -0,0 +1,20 @@
+import os
+import requests
+import json
+
+
+class ANS(object):
+    
+    def __init__(self):
+        self.ans_url = 'https://api.aceinna.com/api'
+
+    def setStatus(self):
+        data = {}
+        url = self.ans_url + "/replaceOrCreate"
+        data_json = json.dumps(data)
+        headers = {'Content-type': 'application/json', 'Authorization' : '' }
+        response = requests.post(url, data=data_json, headers=headers)
+        response = response.json()
+        
+    
+    
---
+++ httpTest/run.py
@@ -0,0 +1,44 @@
+import sys, os
+import json
+from AzureHTTPHelper import HTTPHelper
+from AzureToAns import ANS
+# This is a little class used to abstract away some basic HTTP functionality
+http = HTTPHelper()
+
+# All these print statements get sent to the Azure Functions live log
+print "--- GET ---"
+print http.get
+print
+
+print "--- POST ---"
+print http.post
+print
+
+print "--- HEADERS ---"
+print http.headers
+print
+
+print "--- OTHER ENVIRONMENTAL VARIABLES ---"
+for x in http.env:
+    print x
+print
+
+
+# All data to be returned to the client gets put into this dict
+returnData = {
+    #HTTP Status Code:
+    "status": 200,
+    
+    #Response Body:
+    "body": '{"res":"1222"}',
+    
+    # Send any number of HTTP headers
+    "headers": {
+        "Content-Type": "application/json",
+        "X-Awesome-Header": "YesItIs"
+    }
+}
+
+# Output the response to the client
+output = open(os.environ['res'], 'w')
+output.write(json.dumps(returnData))
